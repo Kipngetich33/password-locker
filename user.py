6fdd96e2f03fac1490954ef007f812783d3b3234
Kipngetich33 <khalifngeno@gmail.com>
2017-12-01 08:26:44 -0500

    create a file user.py and add a class user to it

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    """ This is a class that helps in generating instances of a user
+    """
+    def __init__(self,name,password):
+        list_of_user=[] #this is an empty array that will store the users
+        """ the init method helps in defining properties of our objects"""
+        self.name= name
+        self.password= password
+
+    
